47c5afa9cfc9792e79395e528e4d996874ae9ab2
Elior Abaev <elior.abaev@gmail.com>
2025-04-21 20:09:20 +0300

    Upgrade the utils file

--- backend/password_config.py
+++ backend/password_config.py
@@ -21,3 +21,45 @@ REQUIRE_SPECIAL_CHARS = True
 
 # Allowed special characters
 SPECIAL_CHARS = "!@#$%^&*()_-+=<>?/[]{}|"
+
+# Password history - number of previous passwords to check against
+# User cannot reuse any of their last N passwords
+PASSWORD_HISTORY_LENGTH = 3
+
+# Maximum number of failed login attempts before account is locked
+MAX_LOGIN_ATTEMPTS = 3
+
+# List of common passwords that are not allowed (dictionary passwords)
+DISALLOWED_PASSWORDS = [
+    "password",
+    "123456",
+    "12345678",
+    "qwerty",
+    "abc123",
+    "111111",
+    "123123",
+    "admin",
+    "welcome",
+    "password123",
+    "admin123",
+    "letmein",
+    "monkey",
+    "1234567",
+    "sunshine",
+    "iloveyou",
+    "trustno1",
+    "princess",
+    "123456789",
+    "987654321",
+    "mypassword",
+    "football",
+    "000000",
+    "qwerty123",
+    "dragon",
+    "baseball",
+    "superman",
+    "password1",
+    "internet",
+    "service",
+    "provider"
+]
